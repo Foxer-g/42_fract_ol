164395b42ae560f38070be5e8fa4c5e211c2690d
F0xer <toespino@42angouleme.fr>
2026-02-28 00:54:26 +0000

    update mlx utils

--- src/mlx_utils/window_utils.c
+++ src/mlx_utils/window_utils.c
@@ -6,7 +6,7 @@
 /*   By: toespino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 23:56:13 by toespino          #+#    #+#             */
-/*   Updated: 2026/02/27 22:15:50 by toespino         ###   ########.fr       */
+/*   Updated: 2026/02/28 00:26:39 by toespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ t_mlx	better_mlx_init(void)
 	t_mlx	mlx;
 
 	mlx.mlx = mlx_init();
-	mlx.win = mlx_new_window(mlx.mlx, HEIGHT, LENGHT, WINDOW_NAME);
+	mlx.win = mlx_new_window(mlx.mlx, LENGHT, HEIGHT, WINDOW_NAME);
 	mlx.img.img = mlx_new_image(mlx.mlx, HEIGHT, LENGHT);
 	mlx.img.addr = mlx_get_data_addr(mlx.img.img, &mlx.img.bpp, &mlx.img.ll,
-		&mlx.img.e);
+			&mlx.img.e);
 	return (mlx);
 }
 
@@ -29,7 +29,7 @@ void	put_pixel(t_mlx mlx, int32_t x, int32_t y, int32_t color)
 	char	*dst;
 
 	dst = mlx.img.addr + (y * mlx.img.ll + x * (mlx.img.bpp / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
 void	push_image(t_mlx mlx)
