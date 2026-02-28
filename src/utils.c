84add60f4d384c207fce034d7f9b2ce73fa24182
F0xer <toespino@42angouleme.fr>
2026-02-28 00:54:47 +0000

    update main utils

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: toespino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 21:38:26 by toespino          #+#    #+#             */
-/*   Updated: 2026/02/27 21:45:02 by toespino         ###   ########.fr       */
+/*   Updated: 2026/02/27 23:44:07 by toespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,5 @@ void	error_message(int32_t error_code)
 		write(2, ERROR BAD_AC, (ft_strlen(ERROR) + ft_strlen(BAD_AC)));
 	else
 		write(2, ERROR "What ?\n", (ft_strlen(ERROR) + 7));
+	exit(error_code);
 }
